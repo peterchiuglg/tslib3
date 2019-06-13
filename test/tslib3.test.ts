7853e1929552837b1932e4a7cc046fb17ad42e76
Peter Chiu <peterchiuglg@gmail.com>
2019-06-13 12:29:22 +0800

    0

---
+++ test/tslib3.test.ts
@@ -0,0 +1,18 @@
+import DummyClass from "../src/tslib3"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+
+  it("works",()=>{
+    (new DummyClass()).hello()
+  })
+})
